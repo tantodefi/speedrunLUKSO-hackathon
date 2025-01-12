4dc4ad5c087987c62ccfe04d26b336a4be6faec7
Tantō Defi <107724252+tantodefi@users.noreply.github.com>
2025-01-12 09:07:39 -0600

    add /submit to header nav

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -10,6 +10,7 @@ import {
   ClipboardDocumentListIcon,
   DocumentTextIcon,
   LockClosedIcon,
+  PencilIcon,
 } from "@heroicons/react/24/outline";
 import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
@@ -26,6 +27,11 @@ export const HeaderMenuLinks = () => {
   const { address: connectedAddress } = useAccount();
 
   const menuLinks: HeaderMenuLink[] = [
+    {
+      label: "Submit",
+      href: "/submit",
+      icon: <PencilIcon className="h-4 w-4" />,
+    },
     {
       label: "Submissions",
       href: "/submissions",
