66ad96b0f8731da5f482ff85c5b6500480e47658
Tantō Defi <107724252+tantodefi@users.noreply.github.com>
2025-01-21 04:52:42 -0600

    fix challenged url

--- packages/nextjs/components/extensions-hackathon/HackathonInfo.tsx
+++ packages/nextjs/components/extensions-hackathon/HackathonInfo.tsx
@@ -162,7 +162,7 @@ export const HackathonInfo = () => {
               It&apos;s easy to begin completing the speedrunLUKSO challenges. To begin, simply clone the speedrunLUKSO
               repo using the below command in your terminal:
             </p>
-            <CommandDisplay command="git clone https://github.com/tantodefi/speedrunLUKSO-challenges" />
+            <CommandDisplay command="git clone https://github.com/tantodefi/SpeedRunLUKSO" />
             <p className="mb-4">
               Refer to the <code>readme.md</code> file and use &apos;git checkout&apos; to switch branches between
               different challenges. It&apos;s recommended to complete them in order.
