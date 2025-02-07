0070d5ee59caba71b0aede35a1d9d2886c3f4780
Tantō Defi <107724252+tantodefi@users.noreply.github.com>
2025-02-07 16:20:07 -0600

    fix build

--- packages/nextjs/app/api/submissions/[submissionId]/comments/route.ts
+++ packages/nextjs/app/api/submissions/[submissionId]/comments/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { createComment } from "~~/services/database/repositories/comments";
 import { authOptions } from "~~/utils/auth";
 
-export async function POST(req: Request, { params }: { params: { submissionId: string } }) {
+export async function POST(request: NextRequest, context: { params: { submissionId: string } }) {
   try {
     const session = await getServerSession(authOptions);
 
@@ -11,13 +11,13 @@ export async function POST(req: Request, { params }: { params: { submissionId: s
       return NextResponse.json({ error: "Only admins can add comments" }, { status: 401 });
     }
 
-    const submissionId = parseInt(params.submissionId, 10);
+    const submissionId = parseInt(context.params.submissionId, 10);
 
     if (isNaN(submissionId)) {
       return NextResponse.json({ error: "Invalid submission ID" }, { status: 400 });
     }
 
-    const { comment } = (await req.json()) as { comment: string };
+    const { comment } = (await request.json()) as { comment: string };
 
     if (!comment || comment.length > 255) {
       return NextResponse.json({ error: "Invalid comment submitted" }, { status: 400 });
