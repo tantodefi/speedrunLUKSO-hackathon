38d42c3a631a7ecf3cccffb71888938144915d68
Tantō Defi <107724252+tantodefi@users.noreply.github.com>
2025-02-07 15:14:02 -0600

    fix the build erorr with routes

--- packages/nextjs/app/api/submissions/[submissionId]/comments/route.ts
+++ packages/nextjs/app/api/submissions/[submissionId]/comments/route.ts
@@ -3,17 +3,14 @@ import { getServerSession } from "next-auth";
 import { createComment } from "~~/services/database/repositories/comments";
 import { authOptions } from "~~/utils/auth";
 
-export async function POST(
-  request: NextRequest,
-  { params }: { params: { submissionId: string } },
-): Promise<NextResponse> {
+export async function POST(request: NextRequest, context: { params: Record<string, string> }) {
   try {
     const session = await getServerSession(authOptions);
 
     if (!session?.user.voter) {
       return NextResponse.json({ error: "Only admins can add comments" }, { status: 401 });
     }
-    const submissionId = parseInt(params.submissionId, 10);
+    const submissionId = parseInt(context.params.submissionId, 10);
 
     if (isNaN(submissionId)) {
       return NextResponse.json({ error: "Invalid submission ID" }, { status: 400 });
